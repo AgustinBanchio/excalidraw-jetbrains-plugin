49d6ba574c92a2fd3868ea9d7fd342ce51084532
Agustin Banchio <AgustinBanchio@users.noreply.github.com>
2026-09-19 20:48:52 +0100

    Release 0.2.12

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,7 +41,7 @@ plugins {
 }
 
 group = "com.agustinbanchio"
-version = "0.3.0-dev"
+version = "0.2.12"
 
 val platformVersion = providers.gradleProperty("platformVersion")
 val platformProduct = providers.gradleProperty("platformProduct").orElse("idea")
